77ef45c57d43e268a939c207997429590099b1a6
Giacomo Marinangeli <jibbolo@gmail.com>
2018-11-04 20:02:47 +0100

    Change namespace - Fix fsnotify reference

--- oauthproxy.go
+++ oauthproxy.go
@@ -14,8 +14,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bitly/oauth2_proxy/cookie"
-	"github.com/bitly/oauth2_proxy/providers"
+	"github.com/cloudacademy/oauth2_proxy/cookie"
+	"github.com/cloudacademy/oauth2_proxy/providers"
 	"github.com/mbland/hmacauth"
 )
 
